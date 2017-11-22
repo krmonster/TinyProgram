c80bc7a5807632ba8e221388c861aa65a993cfe5
krmonster <33810621+krmonster@users.noreply.github.com>
2017-11-22 18:38:42 +0800

    Add files via upload

---
+++ Pages/Welcome_0/Welcome_0.js
@@ -0,0 +1,79 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    Text1:"“这是正文”手动阀手动阀阿斯蒂芬阿斯蒂芬阿斯蒂芬阿斯蒂芬阿斯蒂芬阿斯蒂芬阿斯蒂芬风格的活动分工和的风格和反对共和的风格和反对共和阿萨大卫人微软微软豆腐干士大夫敢死队风格的风格和说如果",
+    ImgUrl: [
+      " ../../images/brook.png",
+      " ../../images/grape_iphone6_more.png",
+      " ../../images/iceland_detail.png"
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: "3800",
+    duration: "500",
+    indicatorActiveColor:'#fffbbb',
+    AuthorPortrait_Img:"../../images/third/child_girl.png",
+    Text_Img:"../../images/third/celery.png",
+    Bollection_Img:"../../images/third/collection_unselect.png",
+    AlreadyViewed_Img:"../../images/has_read.png"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
